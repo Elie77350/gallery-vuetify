6342d5d9b89c3aa91ece5072ebe386a7acc35870
Elie BINGUE <eliebingue7@icloud.com>
2023-04-15 21:07:37 +0200

    enregistrement galerie image téléchargeable

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import SideBar from "./components/SideBar.vue";
+import ImageGallery from './components/ImageGallery.vue';
+import { ref } from 'vue';
+
+const isWithColor = ref(true)
+
+const onSelectOption = (isColor: boolean) => {
+  isWithColor.value = isColor;
+}
+</script>
+
+<template>
+  <VApp>
+    <SideBar @selectOption="onSelectOption"/>
+    <VMain>
+      <ImageGallery :isWithColor="isWithColor"/>
+    </VMain>
+  </VApp>
+</template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <VNavigationDrawer expand-on-hover rail>
+    <VList>
+      <VListItem
+        prepend-avatar="src/assets/my-avatar.jpeg"
+        title="Elie BINGUE"
+        subtitle="elie.b3@gmail.com"
+      >
+      </VListItem>
+    </VList>
+  <VDivider />
+
+    <VList density="compact" nav>
+      <VListItem
+        prepend-icon="mdi-image"
+        title="Black/White Gallery"
+        value="color"
+        color="grey"
+        @click="selectOption(false)"
+      />
+      <VListItem
+        prepend-icon="mdi-image"
+        title="Color Gallery"
+        value="color"
+        color="blue-darken-2"
+        @click="selectOption(true)"
+      />
+      </VList>
+  </VNavigationDrawer>
+</template>
+
+
+<script setup lang="ts">
+  // import { defineEmits } from 'vue';
+
+  const emits = defineEmits(['selectOption'])
+
+  const selectOption = (isColor: any) => {
+    emits("selectOption", isColor)
+  }
+</script>
+
+
+
+<style scoped lang="sass">
+
+
+</style>
